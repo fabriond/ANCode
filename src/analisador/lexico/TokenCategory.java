56310b72a59043c8ca8dd64218d61ee4c5e3f101
fabriond <gfbf@ic.ufal.br>
2018-03-27 21:04:20 -0300

    Minor changes

--- src/analisador/lexico/TokenCategory.java
+++ src/analisador/lexico/TokenCategory.java
@@ -3,10 +3,10 @@ package analisador.lexico;
 public enum TokenCategory {
     
 	unknown, main, id, intType, floatType, charType, boolType, stringType, 
-    arrayType, funVoid, funDef, escBegin, escEnd, arrayBegin, arrayEnd, 
-    funReturn, paramBegin, paramEnd, lineEnd, commaSep, loopSep, intCons, 
-    floatCons, charCons, boolCons, stringCons, opAd, opMult, opExp, 
-    opUnNeg, opEq, opConc, opRelEq, opRelLtGt, opLogAnd, opLogOr, opLogNot, 
-    estIf, estElsif, estElse, estLoop, estDo, estWhile, instRead, instPrint;
+	arrayType, funVoid, funDef, escBegin, escEnd, arrayBegin, arrayEnd, 
+	funReturn, paramBegin, paramEnd, lineEnd, commaSep, loopSep, intCons, 
+	floatCons, charCons, boolCons, stringCons, opAd, opMult, opExp, 
+	opUnNeg, opEq, opConc, opRelEq, opRelLtGt, opLogAnd, opLogOr, opLogNot, 
+	estIf, estElsif, estElse, estLoop, estDo, estWhile, instRead, instPrint;
     
 }
